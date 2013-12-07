98dbf2e997364fc4777a4e07492674746e8e7311
Martin Sookael <martinsookael@gmail.com>
2013-12-07 15:07:48 +0200

    tipsy now added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
 $(document).ready(function() {
-	
-
+	$('#url, #message, #contactPerson, #name, #email').tipsy({fade: true, gravity: 'n'});	
+	$('#reason').tipsy({fade: true, gravity: 's'});	
 });
