2b4612eea39568ea8bd7b51adf0e6d865475bf40
Martin Sookael <martinsookael@gmail.com>
2013-12-08 11:38:58 +0200

    removed old legal text

--- app.js
+++ app.js
@@ -239,9 +239,6 @@ function createMail(post) {
 		break;
 	}
 		
-//Andmesubjektil on isikuandmete kaitse seaduse § 11 lõike 4 alusel õigus igal ajal nõuda isikuandmete töötlejalt isikuandmete töötlemise lõpetamist. 
-//Kui Te viidatud andmeid ei eemalda, siis vastavalt isikuandmete kaitse seaduse § 22-le on andmesubjektil õigus pöörduda Andmekaitse Inspektsiooni poole, kellel on õigus veebilehe omaniku suhtes rakendada isikuandmete kaitse seaduse nõuete rikkumise eest sunniraha summas kuni 9600 eurot (isikuandmete kaitse seaduse § 40 lg 2). 
-
 	content += "Palume viidatud andmed viivitamata oma veebilehelt eemaldada, kui nõustute taotlusega. Soovitame Teil andmete eemaldamisel mitte kasutada veakoodi error 404, vaid koodi 451 selgitusega “Seadus piirab ligipääsu” viitega lingile \r\n\http://451.ee/sisu-eemaldamise-viisid/ \r\n\r\nLugege õiguslikku teavet rikkumise kohta aadressil: http://451.ee/juriidiline-analuus\r\n\r\nHindame teie koostööd ja abi!\r\n\r\nParimate soovidega!\r\n\r\nTeie 451\r\n\r\nInstitute of Digital Rights usub, et sõnavabadus internetis saab olla tagatud vaid siis, kui on teada, kes, mis alusel ja millist sisu internetist eemaldab. Instituudi projekti “451″ eesmärk on aidata kaasa sellele, et internetist sisu eemaldamine oleks seaduslik ja dokumenteeritud ning statistika selle kohta avalikult kättesaadav.\r\n\r\n Diplohack Tallinn 2013 raames loodud 451 veebirakendus aitab kaitsta inimeste privaatsust, suurendab ühiskonna teadlikkust ja riigi läbipaistvust ning muudab internetti turvalisemaks ja lugupidavamaks. ";
 	return content;
 }
